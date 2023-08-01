0244cc0aad2bb22e5259c35e01671c4dc12219bd
Prog1910 <shirali.salimov.2002@gmail.com>
2023-08-01 19:25:54 +0400

    Add lesson_1
    https://www.youtube.com/watch?v=fhM0V2N1GpY&list=PLzYkqgWkHPKBcDIP5gzLfASkQyTdy0t4k&index=1&ab_channel=AmichaiMantinband

---
+++ BuberDinner.Application/DependencyInjection.cs
@@ -0,0 +1,12 @@
+﻿using BuberDinner.Application.Services.Authentication;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace BuberDinner.Application;
+
+public static class DependencyInjection
+{
+	public static IServiceCollection AddApplication(this IServiceCollection services)
+	{
+		return services.AddScoped<IAuthenticationService, AuthenticationService>();
+	}
+}
